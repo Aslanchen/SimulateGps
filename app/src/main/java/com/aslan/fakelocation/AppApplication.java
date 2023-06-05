29ff73fff138b684b9a2f9b9de8a818abbdebb0b
Aslanchen <122560007@163.com>
2023-06-05 16:45:29 +0800

    init

---
+++ app/src/main/java/com/aslan/fakelocation/AppApplication.java
@@ -0,0 +1,12 @@
+package com.aslan.fakelocation;
+
+
+import android.app.Application;
+
+public class AppApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
